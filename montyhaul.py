5ecbb1c31078f672e4bfdcb3dcb74cf5fc2cda70
Dan Parshall <parshall.dan@gmail.com>
2016-02-29 00:28:59 -0500

    MontyHaul code

---
+++ montyhaul.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+class trial(object):
+
+	# prize is randomly assigned
+	def __init__(self):
+		self.prize = np.random.randint(0,3)
+
+
+	# user chooses a door to bet on
+	def makeBet(self,bet):
+		assert isinstance(bet,int)
+		assert bet <= 2
+		assert bet >= 0
+		self.bet = bet
+
+
+	# an angel opens one door to reveal a booby prize
+	def openDoor(self):
+		doors = np.array([0,0,0])
+		doors[self.prize] = 1
+		doors[self.bet] = 1
+
+		# angel's choice is between two doors (if user is selecting prize), or
+		# restricted to single door if users door and prize door are different
+		freeDoors = np.where(doors==0)[0]
+		angelDoor = freeDoors[np.random.randint(len(freeDoors))] # door index
+		self.angelDoor = angelDoor
+
+		# value behind the angelDoor should always be zero
+		if doors[angelDoor]:
+			print "THAT WAS SUPPOSED TO BE ZERO :( "
+		return angelDoor
+
+
+	# user may choose to change their bet to the remaining closed door
+	def changeDoor(self,change=False):
+		doors = np.array([0,0,0])
+		doors[self.angelDoor] = 1
+		doors[self.bet] = 1
+
+		option = np.where(doors==0)[0]
+
+		if change:
+			self.bet = option
+
+
+	# If prize is behind user's bet, it's a winner
+	def resolve(self):
+		if self.bet == self.prize:
+			return 1
+		else:
+			return 0
+
+
+wins = 0
+runs = 10000
+for i in range(runs):
+	ordeal = trial()
+	ordeal.makeBet(np.random.randint(3))
+	opened = ordeal.openDoor()
+	ordeal.changeDoor(True)
+	wins += ordeal.resolve()
+
+print wins*1.0/runs
